31d457af305ddbe0472ea033098f5e47ec4dcf42
prateekj117 <prateekj1171998@gmail.com>
2019-03-19 20:25:17 +0530

    Delete weeks from countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
     $(".countdown")
         .countdown("2019/04/11", function(event) {
             $(this).html(
-                event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
+                event.strftime(' <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
             );
         });
 
