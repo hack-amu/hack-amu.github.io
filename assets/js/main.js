95f60c06c56e6e5a913a5c792a066b36de5010d5
prateekj117 <prateekj1171998@gmail.com>
2019-03-19 09:15:51 +0530

    Initial changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
     });
 
     $(".countdown")
-        .countdown("2018/03/01", function(event) {
+        .countdown("2019/04/11", function(event) {
             $(this).html(
                 event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
             );
